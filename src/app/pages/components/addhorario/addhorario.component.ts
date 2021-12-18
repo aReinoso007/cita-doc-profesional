38c59fff038c963a59aecb434d8e5281c0d83615
Alex Reinoso <ralexjessiel@gmail.com>
2021-12-18 02:37:43 -0500

    presentaciones del horario

---
+++ src/app/pages/components/addhorario/addhorario.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addhorario',
+  templateUrl: './addhorario.component.html',
+  styleUrls: ['./addhorario.component.scss'],
+})
+export class AddhorarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
