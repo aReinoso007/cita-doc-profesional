eb946121da98f038201b91b951b08ec210ca81d8
Alex Reinoso <ralexjessiel@gmail.com>
2021-12-20 21:07:31 -0500

    servicios para especialdiad y subespecialidad

---
+++ src/app/service/academia.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { TokenService } from './token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AcademiaService {
+
+  especialidadAPI ='http://localhost:8090/api/public/especialidad';
+  subespecialidadAPI ='http://localhost:8090/api/public/subespecialidad';
+  registroEspeAPI='http://localhost:8090/api/private/medico_especialidad';
+  registroSubEsAPI='http://localhost:8090/api/private/medico_subespecialidad';
+
+  constructor(
+    private tokenService: TokenService,
+    private http: HttpClient
+  ) { }
+
+  
+
+}
