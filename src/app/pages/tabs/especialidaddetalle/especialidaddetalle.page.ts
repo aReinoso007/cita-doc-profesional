ca7d46736ae53987c48d42c3cdd9815d2c01b387
Alex Reinoso <ralexjessiel@gmail.com>
2021-12-26 10:10:42 -0500

    formulario de registro de especialidades

--- src/app/pages/tabs/especialidaddetalle/especialidaddetalle.page.ts
+++ src/app/pages/tabs/especialidaddetalle/especialidaddetalle.page.ts
@@ -1,4 +1,10 @@
+import { ToastController } from '@ionic/angular';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AcademiaService } from './../../../service/academia.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenService } from 'src/app/service/token.service';
+import { RegistroEspecialidad } from 'src/app/model/registroEspecialidad.model';
 
 @Component({
   selector: 'app-especialidaddetalle',
@@ -7,9 +13,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EspecialidaddetallePage implements OnInit {
 
-  constructor() { }
+  especialidades: any[]=[];
+  especialidadFormulario: FormGroup;
+  submitted: boolean = false;
+  registro: RegistroEspecialidad;
+  constructor(private academiaService: AcademiaService, private formBuilder: FormBuilder, private toastCtrl: ToastController, private tokenService: TokenService) { 
+    this.setFormulario();
+  }
 
   ngOnInit() {
+    this.getEspecialidades();
+  }
+
+  getEspecialidades(){
+    this.academiaService.getEspecialidadesDisponibles().subscribe(data=>{
+      this.especialidades = JSON.parse(JSON.stringify(data));
+    })
+  }
+
+  setFormulario(){
+    this.especialidadFormulario = this.formBuilder.group({
+      especialidadId: new FormControl('', Validators.required)
+    })
+  }
+
+  addRegistro(){
+    this.registro = new RegistroEspecialidad(this.tokenService.getUserId(), this.especialidadFormulario.get('especialidadId').value);
+    console.log('Datos a registrar: ', this.registro);
+  }
+
+  onSubmit(){
+    this.submitted = true;
+    if(!this.especialidadFormulario.valid){
+      this.submitted = false;
+      this.presentToastOptions('Error','Seleccione una especialidad');
+    }else{
+      this.addRegistro();
+      this.presentToastOptions('Exito','Especialidad agregada');
+    }
+  }
+  async presentToastOptions(header: string, message: string){
+    const toast = await this.toastCtrl.create({
+      header: header,
+      message: message,
+      position: 'top',
+      duration: 2000
+    });
+    await toast.present();
   }
 
 }
--- src/app/service/academia.service.ts
+++ src/app/service/academia.service.ts
@@ -1,6 +1,9 @@
+import { RegistroEspecialidad } from './../model/registroEspecialidad.model';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TokenService } from './token.service';
+import { Especialidad } from '../model/especialidad.model';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +20,16 @@ export class AcademiaService {
     private http: HttpClient
   ) { }
 
-  
+  getEspecialidadesDisponibles(): Observable<any>{
+    return this.http.get(this.especialidadAPI+'/disponibles/'+ this.tokenService.getUserId());
+  }
+
+  postRegistroEspecialidad(registro: RegistroEspecialidad): Observable<any>{
+    return this.http.post(this.registroEspeAPI, registro);
+  }
+
+  getSubespecialidades(){
+
+  }
 
 }
