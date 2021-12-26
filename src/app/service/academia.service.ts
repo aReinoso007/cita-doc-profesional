7f1d663a490fe272b977b585fc74ecab35cf3546
Alex Reinoso <ralexjessiel@gmail.com>
2021-12-26 10:41:09 -0500

    envio de token con la solicitud

--- src/app/service/academia.service.ts
+++ src/app/service/academia.service.ts
@@ -1,6 +1,6 @@
 import { RegistroEspecialidad } from './../model/registroEspecialidad.model';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TokenService } from './token.service';
 import { Especialidad } from '../model/especialidad.model';
@@ -10,7 +10,7 @@ import { Especialidad } from '../model/especialidad.model';
 })
 export class AcademiaService {
 
-  especialidadAPI ='http://localhost:8090/api/public/especialidad';
+  especialidadAPI ='http://localhost:8090/api/private/especialidad';
   subespecialidadAPI ='http://localhost:8090/api/public/subespecialidad';
   registroEspeAPI='http://localhost:8090/api/private/medico_especialidad';
   registroSubEsAPI='http://localhost:8090/api/private/medico_subespecialidad';
@@ -20,14 +20,25 @@ export class AcademiaService {
     private http: HttpClient
   ) { }
 
+  headers_obj = new HttpHeaders().set("Authorization","Bearer "+this.tokenService.getToken());
+
+  /*Se muestran las especialidades que no han sido registradas por el medico */
   getEspecialidadesDisponibles(): Observable<any>{
-    return this.http.get(this.especialidadAPI+'/disponibles/'+ this.tokenService.getUserId());
+    return this.http.get(this.especialidadAPI+'/disponibles/'+ this.tokenService.getUserId(), {headers: this.headers_obj});
   }
-
+  /*Esto es para guardar el registro de especialidad */
   postRegistroEspecialidad(registro: RegistroEspecialidad): Observable<any>{
-    return this.http.post(this.registroEspeAPI, registro);
+    return this.http.post(this.registroEspeAPI, registro, {headers: this.headers_obj});
   }
 
+  /*Obtener las especialidades registradas por el medico */
+  getEspecialidadesRegistradas(): Observable<any>{
+    const opts = {params: new HttpParams({})}
+    return this.http.get(this.especialidadAPI+'/registradas/'+this.tokenService.getUserId(), {headers: this.headers_obj})
+  }
+
+
+  /*Obtiene las subespecialidades no registradas por el medico */
   getSubespecialidades(){
 
   }
