727ef6dafa018a90353dc7d8e00a2ff7e03a7026
Alex Reinoso <ralexjessiel@gmail.com>
2022-01-11 23:01:15 -0500

    cambios de visualizacion

--- src/app/model/cita.model.ts
+++ src/app/model/cita.model.ts
@@ -1,3 +1,8 @@
+import { Timestamp } from "rxjs/internal/operators/timestamp";
+
 export class Cita{
-    
+    citaId: number;
+    estado: string;
+    fechaCita: string;
+    sintomas: string;
 }
